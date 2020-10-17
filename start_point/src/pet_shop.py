783d7cc64173d24e792585cd2afa3eb784caa5d9
Kenny <kenny.mckeown@gmail.com>
2020-10-17 12:43:27 +0100

    w1 HW initial commit

---
+++ start_point/src/pet_shop.py
@@ -0,0 +1,44 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(list):
+    return list["admin"]["total_cash"]
+
+def add_or_remove_cash(list, cash):
+    list["admin"]["total_cash"] += cash
+
+def get_pets_sold(list):
+    return list["admin"]["pets_sold"]
+
+def increase_pets_sold(list, cash):
+    list["admin"]["pets_sold"] += cash
+    
+def get_stock_count(shop):
+    return len(shop["pets"])
+
+def get_pets_by_breed(shop, type):
+    breed_list = []
+    for pet in shop["pets"]:
+        if pet["breed"] == type:
+            breed_list.append(pet["breed"])
+    return breed_list
+
+def find_pet_by_name(shop, pet_name):
+    for name in shop["pets"]:
+        if name["name"] == pet_name:
+            return name
+
+def remove_pet_by_name(shop, pet_to_remove):
+    for pet in shop["pets"]:
+        if pet["name"] == pet_to_remove:
+            shop["pets"].remove(pet)
+
+
+
+
+
+
+
+    
